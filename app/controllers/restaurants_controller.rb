4bf5e578ef76e73ed2805bf31b9a48068994194f
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-06-07 22:17:10 +0200

    added review model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,12 @@
 class RestaurantsController < ApplicationController
+  validates :name, uniqueness: true
+  before_action :set_restaurant, only: [:show]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -13,15 +15,25 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    # no need for app/views/restaurants/create.html.erb
-    redirect_to restaurant_path(@restaurant)
+
+      if @restaurant.save # Killing 2 birds with 1 stone
+        # Happy path
+        redirect_to restaurant_path(@restaurant)
+      else
+        # Sad sad path
+        render :new
+      end
+
   end
 
   def restaurant_params
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
 
